f9f609252b51bddb400a0b089e7c95f57f38e8ac
lucioacri <lucioacri@gmail.com>
2025-06-17 13:05:50 +0200

    add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import DefaultLayout from "./layouts/defaultLayout";
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <>
-      <h1>webapp</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/films" element={<Films />}>
+            <Route index element={<FilmsList />} />
+            <Route path=":id" element={<ShowFilm />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { Link, NavLink } from "react-router";
+
+export default function Header() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/films">
+                  Films
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
